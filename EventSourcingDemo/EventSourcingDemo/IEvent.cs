b9ade563ddb5a9b224227707b656859dd0052569
liuzhixin405 <164910441@qq.com>
2022-07-17 19:57:06 +0800

    Initial commit

---
+++ EventSourcingDemo/EventSourcingDemo/IEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcingDemo
+{
+    internal interface IEvent { }
+    internal record ProductShipped(String Sku,int Quantity,DateTime DateTime):IEvent;
+    internal record ProductReceived(String Sku, int Quantity, DateTime DateTime) : IEvent;
+    internal record InventoryAdjusted(String Sku, int Quantity, string Resason,DateTime DateTime) : IEvent;
+}
---
+++ EventSourcingDemoTwo/EventSourcingDemoTwo/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcingDemoTwo
+{
+    internal class Order
+    {
+        public string UserId { get; set; }
+        public List<Product> Products { get => new List<Product>(); set => this.Products = value; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+
+    internal class Product
+    {
+        public int Id { get; set; }
+    }
+}
